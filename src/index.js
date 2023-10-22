9538afdb825563f084c217cae98eb04b7ef86dd1
I-Alpha <47956264+I-Alpha@users.noreply.github.com>
2023-10-22 17:37:03 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Suspense>
-    <BrowserRouter basename="/alfa2267.github.io">
+  <Suspense fallback={<div>Loading...</div>}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Suspense>
 );
