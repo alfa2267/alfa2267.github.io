d89af1c85728fd7ef29c9303b10574a08100e326
Oceania Alfa <oceaniaalfa@gmail.com>
2025-08-14 22:24:35 +0100

    Changes to be committed: 	modified:   src/layouts/full/sidebar/Sidebar.js

--- src/layouts/full/sidebar/Sidebar.js
+++ src/layouts/full/sidebar/Sidebar.js
@@ -1,7 +1,6 @@
 import { useMediaQuery, Box, Drawer } from '@mui/material';
 import Logo from '../shared/logo/Logo';
 import SidebarItems from './SidebarItems';
-import { Upgrade } from './Updrade';
 
 const Sidebar = (props) => {
 
